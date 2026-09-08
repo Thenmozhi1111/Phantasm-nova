2fe68dc5ad3640307c70e4d6044ea0d64aa3836e
Thenmozhi1111 <thenurajendiran@gmail.com>
2026-09-08 12:20:28 +0530

    Remove server listening logic from server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,4 @@ import app from './app.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Backend server running on http://localhost:${PORT}`);
-});
+export default app;
